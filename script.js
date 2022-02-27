07bbde0c7125e30e995cb7cd05635776564eba14
Yourdad <mka498@mail.ru>
2022-02-27 13:46:58 +0300

    jsumid

---
+++ script.js
@@ -0,0 +1,19 @@
+var str = prompt("Ismingizni yozing");
+console.log("Mening ismim "+ str);
+var num = +prompt("Yoshingiz")
+console.log ("Men "+num+ " yoshdaman ")
+var num1 = +prompt("6 + 3 = ? ");
+console.log(num1);
+var num2 = +prompt("6 - 3 = ? ");
+console.log(num2);
+var num3 = +prompt("6 * 3 = ? ");
+console.log(num3);
+var num4 = +prompt("6 / 3 = ? ");
+console.log(num4);
+var num5 = +prompt("10 % 3 = ? ");
+console.log(num5);
+console.log("Misol 1: 6 + 3 = " + num1 + " Sizning javob " + num1);
+console.log("Misol 2: 6 - 3 = " + num2 + " Sizning javob " + num2);
+console.log("Misol 3: 6 * 3 = " + num3 + " Sizning javob " + num3);
+console.log("Misol 4: 6 / 3 = " + num4 + " Sizning javob " + num4);
+console.log("Misol 5: 10 % 3 = " + num5 + " Sizning javob " + num5);
